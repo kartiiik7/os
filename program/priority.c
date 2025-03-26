f2a7eb31e12190638ec57b544c1a4df4b46c8b9a
kartiiik7 <kartikanil2004@gmail.com>
2025-03-26 12:58:43 +0530

    non preemptive priority.c

--- program/priority.c
+++ program/priority.c
@@ -1,74 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+
 struct Process
 {
- int id,at,bt,ct,tat,wt,rt,pt;
+    int id, at, bt, ct, tat, wt, pt;
+    int completed; // To track completion
 };
-int findshortest(struct Process p[],int n,int currentTime)
+
+int findHighestPriority(struct Process p[], int n, int currentTime)
 {
- int index=-1,pt=INT_MAX;
- for(int i=0;i<n;i++)
- {
-  if(p[i].at<=currentTime&&p[i].rt!=0)
-  {
-   if(p[i].pt<pt)
-   {
-    pt=p[i].pt;
-    index=i;
-   }
-  }
- }
- return index;
-}
-void Srtf(struct Process p[],int n)
-{
-int completed=0,currentTime=0;
-float tot_tat=0,tot_wt=0;
-while(completed<n)
-{
-int ind=findshortest(p,n,currentTime);
-if(ind==-1)
-{
-currentTime++;
-}
-else{
-p[ind].rt--;
-currentTime++;
-if(p[ind].rt==0)
-{
-completed++;
-p[ind].ct=currentTime;
-p[ind].tat=p[ind].ct-p[ind].at;
-p[ind].wt=p[ind].tat-p[ind].bt;
-tot_tat+=p[ind].tat;
-tot_wt+=p[ind].wt;
-}
-}
-}
-printf("PID\tAT\tBT\tCT\tTAT\tWT\tPT\n");
-for(int i=0;i<n;i++)
-{
-printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",p[i].id,p[i].at,p[i].bt,p[i].ct,p[i].tat,p[i].wt,p[i].pt);
-}
-printf("Average TAT=%f\n",tot_tat/n);
-printf("Average WT=%f\n",tot_wt/n);
-}
-void main()
-{
-int n;
-printf("Enter the number of process:");
-scanf("%d",&n);
-struct Process p[n];
-printf("Enter the Arrival time(AT) and Burst Time(BT) and Priority(P) \n");
-for(int i=0;i<n;i++)
-{
-p[i].id=i+1;
-printf("P%d(AT,BT,P):",p[i].id);
-scanf("%d%d%d",&p[i].at,&p[i].bt,&p[i].pt);
-p[i].rt=p[i].bt;
-}
-Srtf(p,n);
+    int index = -1, highestPriority = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (p[i].at <= currentTime && !p[i].completed) // Process must have arrived and not be completed
+        {
+            if (p[i].pt < highestPriority || (p[i].pt == highestPriority && p[i].at < p[index].at))
+            {
+                highestPriority = p[i].pt;
+                index = i;
+            }
+        }
+    }
+    return index;
 }
 
+void NonPreemptivePriority(struct Process p[], int n)
+{
+    int completedCount = 0, currentTime = 0;
+    float totalTAT = 0, totalWT = 0;
 
+    while (completedCount < n)
+    {
+        int ind = findHighestPriority(p, n, currentTime);
+
+        if (ind == -1) // If no process is available, move time forward
+        {
+            currentTime++;
+        }
+        else
+        {
+            // Process execution
+            p[ind].completed = 1;
+            completedCount++;
+            currentTime += p[ind].bt;
+            p[ind].ct = currentTime;
+            p[ind].tat = p[ind].ct - p[ind].at;
+            p[ind].wt = p[ind].tat - p[ind].bt;
+            totalTAT += p[ind].tat;
+            totalWT += p[ind].wt;
+        }
+    }
+
+    printf("PID\tAT\tBT\tP\tCT\tTAT\tWT\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].id, p[i].at, p[i].bt, p[i].pt, p[i].ct, p[i].tat, p[i].wt);
+    }
+
+    printf("Average TAT = %.2f\n", totalTAT / n);
+    printf("Average WT = %.2f\n", totalWT / n);
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    struct Process p[n];
+
+    printf("Enter Arrival Time (AT), Burst Time (BT), and Priority (P) for each process:\n");
+    for (int i = 0; i < n; i++)
+    {
+        p[i].id = i + 1;
+        p[i].completed = 0; // Initially, no process is completed
+        printf("P%d (AT, BT, P): ", p[i].id);
+        scanf("%d %d %d", &p[i].at, &p[i].bt, &p[i].pt);
+    }
+
+    NonPreemptivePriority(p, n);
+    return 0;
+}
